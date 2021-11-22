3997538993edd16debe8d6e2dd88498b2ea0e50b
Joel Maciel <soldado25461@gmail.com>
2021-11-22 18:46:16 -0300

    Listando por datas

--- src/index.js
+++ src/index.js
@@ -78,5 +78,18 @@ app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation);
   return response.status(201).send();
 });
+app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.body;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement =
+    customer.statement.filter((statement) =>
+      statement.created_at.toDateString()
+    ) === new Date(dateFormat).toDateString();
+
+    return response.json(customer.statement);
+});
 
 app.listen(3333);
